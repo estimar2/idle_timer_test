b24a92d51ca7391f092f19d6cc68f545bdae45d4
estimar2 <songsm5504@gmail.com>
2023-12-18 17:39:48 +0900

    [23.12.18] modal 추가

--- src/components/IdleTimerContainer.js
+++ src/components/IdleTimerContainer.js
@@ -1,19 +1,46 @@
-import React, { useId, useRef } from "react";
+import React, { useState, useRef } from "react";
 import IdleTimer from "react-idle-timer";
+import { Modal } from "antd";
 
 const IdleTimerContainer = () => {
   const idleTimerRef = useRef(null);
 
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   const _onIdle = () => {
-    console.log("user is idle");
+    setModalIsOpen(true);
+    console.log("You are idle for 5 seconds");
+  };
+
+  const logOut = () => {
+    setModalIsOpen(false);
+    setIsLoggedIn(false);
+    console.log("User has logged out");
+  };
+
+  const stayLoggedIn = () => {
+    setModalIsOpen(false);
+    console.log("User is active");
   };
 
   return (
-    <IdleTimer
-      ref={idleTimerRef}
-      timeout={5 * 1000}
-      onIdle={_onIdle}
-    ></IdleTimer>
+    <>
+      {isLoggedIn ? <h2> Hello </h2> : <h2> You are logged out</h2>}
+
+      <Modal title="modal" visible={modalIsOpen}>
+        <h2> You have been Idle for a while</h2>
+        <p>You'll be logged out soon</p>
+        <button onClick={logOut}>Log me out</button>
+        <button onClick={stayLoggedIn}>Keep me signed in</button>
+      </Modal>
+
+      <IdleTimer
+        ref={idleTimerRef}
+        timeout={5 * 1000}
+        onIdle={_onIdle}
+      ></IdleTimer>
+    </>
   );
 };
 
